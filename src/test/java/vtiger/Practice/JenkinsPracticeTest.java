02054a2bd5455cba1df76e886205ddecc1c9a9cf
Admin <Admin@DESKTOP-77FO7BT>
2022-09-16 17:36:47 +0530

    commit and push for practice

---
+++ src/test/java/vtiger/Practice/JenkinsPracticeTest.java
@@ -0,0 +1,13 @@
+package vtiger.Practice;
+
+import org.testng.annotations.Test;
+
+public class JenkinsPracticeTest 
+{
+	@Test
+	public void practiceTest()
+	{
+		System.out.println("hello world");
+	}
+
+}
